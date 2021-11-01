9bd063b4f42d11a27956a9fa39b381503ee990b9
Ryan Fitzgerald <ryfitz@ymail.com>
2021-10-31 23:50:23 -0600

    done

--- app/Services/WeatherService.js
+++ app/Services/WeatherService.js
@@ -7,6 +7,8 @@ class WeatherService {
     let F = ((K - 273.15) * 9 / 5 + 32).toFixed(0);
     console.log(C, F);
     document.getElementById("city").innerText = res.data.name
+    document.getElementById("weather").innerText = res.data.weather[0].description
+
     document.getElementById("temp").innerHTML = `${F}° F`
 
     document.getElementById("temp").onclick = changeTemp
